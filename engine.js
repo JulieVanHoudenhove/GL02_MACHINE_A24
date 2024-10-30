87718dbd043c54dbb1a94c03221bbc8d5434f1d2
/dev/input/arthur <59390256+dev-err418@users.noreply.github.com>
2024-10-30 17:46:04 +0100

    Update engine.js

--- engine.js
+++ engine.js
@@ -3,7 +3,8 @@ var Engine = {
         console.log("Engine Ready !")
         console.log("Magenta ok".magenta)
       	console.log("Cyan level checked".cyan)
-        console.log("Blue level checked".blue)     
+        console.log("Blue level checked".blue)   
+        console.log("Gray level checked".gray)
         return true
     }
 }
